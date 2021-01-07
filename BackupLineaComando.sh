553037db8ee96840a05511b80a08778193b5749f
root <root@atower.labrhel8>
2021-01-07 15:44:20 -0800

    Codificación de Backup de Spectrum

--- BackupLineaComando.sh
+++ BackupLineaComando.sh
@@ -5,7 +5,7 @@ git add BackupSpectrum.v10.4.yml
 git commit -m "Codificación de Backup de Spectrum"
 
 echo "Ejecutar playbook para el backup de spectrum"
-ansible-playbook BackupSpectrum.v10.4.yml -vvv --extra-vars "hostname_name=localhost.localdomain hostname__pretty=localhost redhat_user=jzavala74mz redhat_pass=V4l3r1a20 host_user=jzavala74mz host_pass=P4ssw0rd host_name=192.168.112.133  and flag_suscribir=NO"
+ansible-playbook BackupSpectrum.v10.4.yml -vvv --extra-vars "hostname_name=localhost.localdomain ruta_carpeta_backups=/home/jzavala/respaldos/Spectrum"
 echo "listo !!"
 
 git push
